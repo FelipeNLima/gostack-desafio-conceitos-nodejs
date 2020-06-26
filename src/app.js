7307563cc54d5730c39e98c42798271dede50ab4
Felipe Lima <felipinho.lima27@gmail.com>
2020-06-26 19:45:08 -0300

    Desafio conceito de Node.Js

---
+++ src/app.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid, isUuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+function validateRepositoriesId(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: "Invalid repository ID."});
+  }
+
+  return next();
+}
+
+app.use('/repositories/:id', validateRepositoriesId);
+
+app.get("/repositories", (request, response) => {
+  const { title } = request.query;
+
+  const results = title ? repositories.filter(repo => repo.title.includes(title)) : repositories;
+  return response.json(results);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repositorie = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoriesIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repositoriesIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exists.' });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoriesIndex].likes,
+  };
+
+  repositories[repositoriesIndex] = repository;
+
+  return response.json(repository);
+
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const repositoriesIndex = repositories.findIndex(repo => repo.id === id);
+
+  repositories.splice(repositoriesIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+  const repository = repositories.find(repo => repo.id === id);
+
+  repository.likes += 1;
+
+  return response.json(repository);
+});
+
+module.exports = app;
